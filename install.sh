7287ecdb80b37eac80052aeb768aa47311b34686
Peter Milne <milnepe@gmail.com>
2024-04-04 11:46:31 +0100

    Enable venv install

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
-#! /usr/bin/sh
+#! /usr/bin/bash
+
 # Install all dependencies and setup radioglobe service to run under default user
-sudo apt install vlc pulseaudio python3-pip python3-smbus python3-dev python3-rpi.gpio
+
+sudo apt install vlc
+# sudo apt install vlc pulseaudio python3-pip python3-smbus python3-dev python3-rpi.gpio
+
+# Create python virtual environment
+echo "Creating virtual environment..."
+python -m venv venv
+source ./venv/bin/activate
+
+# Install python dependencies
+pip install spidev
+pip install smbus
+pip install RPi.GPIO
 pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
-pip3 install python-vlc
-pip3 install spidev
+#pip3 install python-vlc
+
 # Set paths according to username
 sed -i "s/USER/${USER}/g" services/*.service
 sudo cp services/*.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl start radioglobe.service
 sudo systemctl enable radioglobe.service
+sudo systemctl start radioglobe.service
