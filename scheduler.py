a23439224362282f8531dabff3a10d9842f3cdd3
Peter Milne <milnepe@gmail.com>
2024-04-04 18:07:59 +0100

    Lint scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,3 @@
-#! /usr/bin/python3
 import time
 import threading
 
@@ -6,69 +5,70 @@ EXPIRY = 0
 RELOAD = 1
 CALLBACK = 2
 
+
 class Scheduler (threading.Thread):
-  def __init__(self, threadID, name):
-    threading.Thread.__init__(self)
-    self.threadID = threadID
-    self.name = name
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
 
-    self.time = 0
-    self.timers = []
+        self.time = 0
+        self.timers = []
 
-  def attach_timer(self, callback:callable, initial_value_sec:int, one_shot:bool=True):
-    # Overwrite existing timer for the same callback
-    for exist_timer in self.timers:
-      if exist_timer[CALLBACK] == callback:
-        exist_timer[EXPIRY] = self.time + initial_value_sec
+    def attach_timer(self, callback: callable, initial_value_sec: int, one_shot=True: bool):
+        # Overwrite existing timer for the same callback
+        for exist_timer in self.timers:
+            if exist_timer[CALLBACK] == callback:
+                exist_timer[EXPIRY] = self.time + initial_value_sec
+                if not one_shot:
+                    new_timer[RELOAD] = initial_value_sec
+                return
+
+        # Else make a new timer
+        new_timer = [None, None, None]
+        new_timer[EXPIRY] = self.time + initial_value_sec
+        new_timer[CALLBACK] = callback
         if not one_shot:
-          new_timer[RELOAD] = initial_value_sec
-        return
+            new_timer[RELOAD] = initial_value_sec
+        self.timers.append(new_timer)
 
-    # Else make a new timer
-    new_timer = [None, None, None]
-    new_timer[EXPIRY] = self.time + initial_value_sec
-    new_timer[CALLBACK] = callback
-    if not one_shot:
-      new_timer[RELOAD] = initial_value_sec
-    self.timers.append(new_timer)
+    def run(self):
+        while True:
+            time.sleep(1)
+            self.time += 1
 
-  def run(self):
-    while True:
-      time.sleep(1)
-      self.time += 1
+            # Loop all the timers to see if they've expired.  Call their callback if they have, then delete if they're
+            # one shot timers
+            num_timers = len(self.timers)
+            timer = 0
+            while timer < num_timers:
+                if self.time >= self.timers[timer][EXPIRY]:
+                    self.timers[timer][CALLBACK]()
+                    if self.timers[timer][RELOAD]:
+                        self.timers[timer][EXPIRY] = self.time + self.timers[timer][RELOAD]
+                    else:
+                        self.timers.__delitem__(timer)
+                        num_timers -= 1
+                timer += 1
 
-      # Loop all the timers to see if they've expired.  Call their callback if they have, then delete if they're
-      # one shot timers
-      num_timers = len(self.timers)
-      timer = 0
-      while timer < num_timers:
-        if self.time >= self.timers[timer][EXPIRY]:
-          self.timers[timer][CALLBACK]()
-          if self.timers[timer][RELOAD]:
-            self.timers[timer][EXPIRY] = self.time + self.timers[timer][RELOAD]
-          else:
-            self.timers.__delitem__(timer)
-            num_timers -= 1
-        timer += 1
 
 if __name__ == "__main__":
-  def print_one_shot():
-    print("One shot")
+    def print_one_shot():
+        print("One shot")
 
-  def print_reload():
-    print("Reload")
-  
-  scheduler = Scheduler(1, "SCHEDULER")
-  scheduler.start()
+    def print_reload():
+        print("Reload")
 
-  # The first one shouldn't expire, because the second overwrites it!
-  scheduler.attach_timer(print_one_shot, 5)
-  scheduler.attach_timer(print_one_shot, 8)
-  scheduler.attach_timer(print_reload, 6, one_shot=False)
+    scheduler = Scheduler(1, "SCHEDULER")
+    scheduler.start()
 
-  try:
-    while True:
-      time.sleep(0.5)
+    # The first one shouldn't expire, because the second overwrites it!
+    scheduler.attach_timer(print_one_shot, 5)
+    scheduler.attach_timer(print_one_shot, 8)
+    scheduler.attach_timer(print_reload, 6, one_shot=False)
 
-  except:
-    exit()
+    try:
+        while True:
+            time.sleep(0.5)
+    except Exception:
+        exit()
