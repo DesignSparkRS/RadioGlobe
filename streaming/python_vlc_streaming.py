b1fde20d0566110acdd438cbaced02fa50f8bcd2
Peter Milne <milnepe@gmail.com>
2024-04-15 18:31:14 +0100

    Maintain single Streamer

--- streaming/python_vlc_streaming.py
+++ streaming/python_vlc_streaming.py
@@ -4,75 +4,78 @@ import logging
 import vlc
 
 
-class Streamer ():
+class Streamer():
     """
     A streaming audio player using python-vlc
     This improves handling of media list (pls and m3u's) streams
     """
-
-    def __init__(self, audio, url):
+    def __init__(self, audio='alsa', url=None):
         logging.debug(f"Starting Streamer: {audio}, {url}")
         self.audio = audio
         self.url = url
-        self.player = None
+        self.instance = vlc.Instance()
+        self.media = None
+        self.mediaplayer = None
+        self.is_playing = False
 
+    def play(self, url):
         playlists = set(['pls', 'm3u'])
-        Instance = vlc.Instance()
-
-        url = self.url.strip()
+        url = url.strip()
         logging.debug(f"Playing URL {url}")
+
+        if self.is_playing:
+            self.stop()
+        self.is_playing = True
 
         # We need a different type of media instance for urls containing playlists
         extension = (url.rpartition(".")[2])[:3]
         logging.debug(f"Extension: {extension}")
         if extension in playlists:
             logging.debug(f"Creating media_list_player...")
-            player = Instance.media_list_player_new()
-            media = Instance.media_list_new([url])
-            player.set_media_list(media)
+            self.mediaplayer = self.instance.media_list_player_new()
+            self.media = self.instance.media_list_new([url])
+            self.mediaplayer.set_media_list(self.media)
         else:
             logging.debug(f"Creating media_player...")
-            player = Instance.media_player_new()
-            media = Instance.media_new(url)
-            player.set_media(media)
+            self.mediaplayer = self.instance.media_player_new()
+            self.media = self.instance.media_new(url)
+            self.mediaplayer.set_media(self.media)
 
-        self.player = player
-
-    def play(self):
-        self.player.play()
+        self.mediaplayer.play()
 
     def stop(self):
-        self.player.stop()
+        self.is_playing = False
+        self.mediaplayer.stop()
 
 
 if __name__ == "__main__":
-    """python python-vlc-streaming.py ../stations.json"""
+    """python python_vlc_streaming.py ../json/london-stations.json"""
     import sys
     import files
+
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    # logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.DEBUG)
 
     clip_duration = 10
-    audio = 'alsa'
 
     stations_file = sys.argv[1]
     stations = files.load_stations(stations_file)
 
     # Get list of stations
-    station_list = []
+    stations_list = []
     for k, v in stations.items():
         for v in v['urls']:
-            station_list.append(v)
+            stations_list.append(v)
 
-    logging.debug(station_list)
-    logging.info(f"Station list length: {len(station_list)} URLs")
+    logging.debug(stations_list)
+    logging.info(f"Station list length: {len(stations_list)} URLs")
 
-    for i, station in enumerate(station_list):
-        logging.info(f"Playing URL {i}, {station['name']}, {station['url']}")
-        player = Streamer(audio, station['url'])
-        player.play()
+    player = Streamer()
+    for i, station in enumerate(stations_list):
+        url = station['url']
+        logging.info(f"Playing URL {i}, {station['name']}, {url}")
+        player.play(url)
         time.sleep(clip_duration)
-        player.stop()
 
     logging.info("End of list")
