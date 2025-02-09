90c57f0e4c1509ac44d3b157644598c2dd72de12
vfinel <victor.finel.pro@gmail.com>
2025-02-09 15:14:52 +0100

    fix gpio installation

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/sh
 # Install all dependencies and setup radioglobe service to run under default user
-sudo apt install vlc pulseaudio python3-pip python3-smbus python3-dev python3-rpi.gpio
+sudo apt install vlc pulseaudio python3-pip python3-smbus python3-dev python3-rpi-lgpio
 pip3 install https://github.com/pl31/python-liquidcrystal_i2c/archive/master.zip
 pip3 install python-vlc
 pip3 install spidev
+
 # Set paths according to username
 sed -i "s/USER/${USER}/g" services/*.service
 sudo cp services/*.service /etc/systemd/system
